8a892cafc2442a82dd2f46cdd67fdff0b2e935e9
Ruthie Irvin <ruth.diane.irvin@gmail.com>
2023-02-21 16:25:33 -0600

    adds CompletionRecordForm component instance

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import ParticipantDetails from './components/ParticipantDetails';
+import CompletionRecordForm from './components/CompletionRecordForm';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <ParticipantDetails />
+        <CompletionRecordForm />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/CompletionRecordForm.js
@@ -0,0 +1,7 @@
+const CompletionRecordForm = () => {
+    return(
+        <div>Hi! This is the CompletionRecordForm component in action.</div>
+    )
+}
+
+export default CompletionRecordForm;
