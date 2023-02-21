7416ba7f283fef81ac63b5ea2a3937dc3e705b66
Ruthie Irvin <ruth.diane.irvin@gmail.com>
2023-02-21 16:48:32 -0600

    adds participant email form

--- src/components/ParticipantDetails.js
+++ src/components/ParticipantDetails.js
@@ -3,6 +3,10 @@ const ParticipantDetails = (props) => {
         <>
         <div>Hello, {props.particpant}!</div>
         <div>Hi! This is the ParticipantDetails component in action.</div>
+        <form className="participant-email">
+            <input type="text" placeholder="Please enter particpant email"/>
+            <input type="Submit"/>
+        </form>
         </>
     )
 }
