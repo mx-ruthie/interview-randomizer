8f2a90d1eda40c3839e5ad8a1ff2d726e7f4496e
Ruthie Irvin <ruth.diane.irvin@gmail.com>
2023-02-21 17:04:08 -0600

    adds onSubmit handleSubmit function which calls e.target[0]

--- src/components/ParticipantDetails.js
+++ src/components/ParticipantDetails.js
@@ -1,10 +1,18 @@
 const ParticipantDetails = (props) => {
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target[0].value);
+
+    }
     return(
         <>
         <div>Hello, {props.particpant}!</div>
         <div>Hi! This is the ParticipantDetails component in action.</div>
-        <form className="participant-email">
-            <input type="text" placeholder="Please enter particpant email"/>
+        <form className="participant-email" onSubmit={handleSubmit}>
+            {/* on following line "participant-email" in all lower case with hyphen--will use this
+            to call the data later, so having the formatting with no spaces, etc is needed */}
+            <input type="text" placeholder="Please enter particpant email" name="participant-email"/>
             <input type="Submit"/>
         </form>
         </>
