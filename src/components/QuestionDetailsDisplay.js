ad307fced7dbae8d165ed75652ff4ce84b6d34e3
Ruthie Irvin <ruth.diane.irvin@gmail.com>
2023-02-24 16:33:44 -0600

    starting to play with more elements of the components and css

--- src/components/QuestionDetailsDisplay.js
+++ src/components/QuestionDetailsDisplay.js
@@ -1,6 +1,12 @@
 const QuestionDetailsDisplay = () => {
     return(
-        <div>Hi! This is the QuestionDetailsDisplay component in action.</div>
+        <div>
+            <p className="example">Once a question has been randomly selected onClick, it will display here</p>
+            <p className="example">It will have the Category</p>
+            <p className="example">and difficulty</p>
+            <p className="example">and question itself</p>
+            <p className="example">Might there also be a solution provided?</p>
+        </div>
     )
 }
 
