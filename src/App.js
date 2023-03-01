a4f0c2cab71422c9cfde95f7f0a9dd407b262585
Ruthie Irvin <ruth.diane.irvin@gmail.com>
2023-03-01 14:46:55 -0600

    adds minor changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import ParticipantDetails from './components/ParticipantDetails';
 import CompletionRecordForm from './components/CompletionRecordForm';
