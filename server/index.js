0f9c58bdd3251690df9bcb960741da45cdc89ec9
Ruthie Irvin <ruth.diane.irvin@gmail.com>
2023-03-23 16:49:10 -0500

    adds backend

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 8088;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.json("hello from techtonica");
+});
+
+app.listen(PORT, () => console.log(`Hola! Server is running on port ${PORT}`));
