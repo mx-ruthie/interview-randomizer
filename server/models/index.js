94a108cd5a05245c8075842ec0e349a7de7a545c
Ruthie Irvin <ruth.diane.irvin@gmail.com>
2023-03-23 17:09:06 -0500

    adds link to tutorial on models/index.js

---
+++ server/models/index.js
@@ -0,0 +1,1 @@
+//at this phase of this tutorial on save https://www.robinwieruch.de/postgres-express-setup-tutoria%20wil/#:~:text=To%20connect%20PostgreSQL%20to%20your,convert%20data%20among%20incompatible%20types
