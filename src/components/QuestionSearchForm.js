5184d809fc3db1de374d98a5497e92c63d1053a8
Ruthie Irvin <ruth.diane.irvin@gmail.com>
2023-05-04 19:46:01 -0500

    cleaning up code

--- src/components/QuestionSearchForm.js
+++ src/components/QuestionSearchForm.js
@@ -1,54 +1,44 @@
 //import fakeData from '../fixtures/questions.json';
 import { useState } from "react";
 
-
-
-
 const QuestionSearchForm = (props) => {
- 
-const [questionText, setQuestionText] = useState("The question text will generate here.")
-const [testcases, setTestcases] = useState(["Testcase 1", "Testcase 2", "Testcase 3"]);
+  const [questionText, setQuestionText] = useState(
+    "The question text will generate here."
+  );
+  const [testcases, setTestcases] = useState([
+    "Testcase 1",
+    "Testcase 2",
+    "Testcase 3",
+  ]);
 
-props.setQuestion(questionText);
-    props.setTestCases(testcases);
+  // moved these props outside of the handleSubmit so that they would generate before submitting 
+  props.setQuestion(questionText);
+  props.setTestCases(testcases);
 
-async function fetchQuestionData () {
-  const response = await fetch("http://localhost:8088/questions");
-  const questionJSON = await response.json();
-  setQuestionText(questionJSON[0].text);
-  
-  setTestcases(questionJSON[0].testCases);
-  return questionJSON;
-}
-console.log(questionText);
-console.log(testcases);
+  async function fetchQuestionData() {
+    const response = await fetch("http://localhost:8088/questions");
+    const questionJSON = await response.json();
+    //setting part of the results to state so that the results could be accessed outside of the fetch function
+    setQuestionText(questionJSON[0].text);
+    setTestcases(questionJSON[0].testCases);
+    return questionJSON;
+  }
+  console.log(questionText);
+  console.log(testcases);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("inside the handleSubmit")
-   fetchQuestionData();
-   
-    
-    };
+    fetchQuestionData();
+  };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        {/* <label>
-          <select>
-            <option value="strings">Strings</option>
-            <option value="Arrays">Arrays</option>
-            <option value="booleans">Booleans</option>
-            <option value="integers">Integers</option>
-            <option value="objects">Objects</option>
-          </select>
-        </label> */}
         {/* edited the submit to be a button instead of an input type */}
-        <button type="submit" value="Randomize">
-        Randomize
-      </button>
+        <button type="submit" value="Click for Question">
+          Click for Question
+        </button>
       </form>
-      
     </>
   );
 };
