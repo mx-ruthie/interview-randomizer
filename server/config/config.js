6db540e349087e900358523c69fc7908d520ed69
Ruthie Irvin <ruth.diane.irvin@gmail.com>
2023-05-19 14:19:09 -0500

    updates config file with port and db host variables

--- server/config/config.js
+++ server/config/config.js
@@ -10,15 +10,16 @@ module.exports = {
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE,
-    host: '127.0.0.1',
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
     dialect: 'postgres',
   },
-  // production: {
-  //   username: process.env.DATABASE_USER,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   database: process.env.DATABASE,
-  //   // host: process.env.DB_HOSTNAME,
-  //   // port: process.env.DB_PORT,
-  //   dialect: 'postgres'
-  // }
+  production: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dialect: 'postgres'
+  }
 };
